2074a9f965644767604dd8f985c214c030f352ed
Mathesh <matheshwar.srinivasan@gmail.com>
2025-06-03 18:28:44 +0530

    Create variables.tf

---
+++ terraform/vpc/variables.tf
@@ -0,0 +1,23 @@
+variable "name" {}
+variable "cidr" {}
+variable "azs" {
+  type = list(string)
+}
+variable "public_subnets" {
+  type = list(string)
+}
+variable "private_subnets" {
+  type = list(string)
+}
+variable "enable_nat_gateway" {
+  type = bool
+}
+variable "single_nat_gateway" {
+  type = bool
+}
+variable "enable_dns_hostnames" {
+  type = bool
+}
+variable "tags" {
+  type = map(string)
+}
